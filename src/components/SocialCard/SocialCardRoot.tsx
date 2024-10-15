97391ff2ee4eb021d844a174eeaff133d73da89b
danielcosta0701 <contatodanielskt@gmail.com>
2024-10-14 23:23:08 -0300

    feat: add location and change colors

--- src/components/SocialCard/SocialCardRoot.tsx
+++ src/components/SocialCard/SocialCardRoot.tsx
@@ -12,7 +12,7 @@ export default function SocialCardRoot(props: SocialCardRootProps) {
     } = props;
 
     return (
-        <div className={`min-w-80 max-w-96 flex flex-col gap-5 p-6 bg-neutral-800 rounded-lg shadow-sm ${className}`}>
+        <div className={`min-w-80 max-w-96 flex flex-col gap-5 p-6 bg-gray-800 rounded-lg shadow-sm ${className}`}>
             { children ?? "Sem conteúdo" }
         </div>
     );
